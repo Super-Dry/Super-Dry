30188af56254b101d9ed34b42594885cbc9fada6
Kenny Chen <kennygchen@yahoo.com>
2023-05-11 11:07:12 -0700

    enemies take damage

--- Assets/Scripts/EnemyAction.cs
+++ Assets/Scripts/EnemyAction.cs
@@ -27,16 +27,28 @@ public class EnemyAction : MonoBehaviour
     public Transform shootPoint;
     public float bulletSpeed;
 
+    //Health
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public Healthbar healthbar;
+
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         fov = GetComponent<FieldOfView>();
         agent = GetComponent<NavMeshAgent>();
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
         if(fov.canSeePlayer && !fov.canAttackPlayer)
         {
             ChasePlayer();
@@ -112,5 +124,11 @@ public class EnemyAction : MonoBehaviour
     private void ResetAttack()
     {
         alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.SetHealth(currentHealth);
     }
 }
