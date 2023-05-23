63f99104ba0147ca5b917f4a728f8079819cdcbf
Abdullah Chaudhry <achau048@ucr.edu>
2023-05-23 00:16:46 -0700

    Healing now gradually occurs over time

--- Assets/Scripts/WaterScript.cs
+++ Assets/Scripts/WaterScript.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class WaterScript : MonoBehaviour
 {
-    float _interval = 30f;
+    float _interval = .1f;
+    float _time = 0f;
     
-    private void OnTriggerEnter(Collider c){
+    private void OnTriggerStay(Collider c){
         if(c.gameObject.name == "CactusGuy"){
+
             Debug.Log("please work for the love of god");
-            c.gameObject.GetComponent<CactusGuy>().HealShield(5);
+
+            var player = c.gameObject.GetComponent<CactusGuy>();
+            _time += Time.deltaTime;
+            Debug.Log(_time);
+            if(_time >= _interval){
+                Debug.Log("healing!");
+                player.HealShield(1);
+                _time -= _interval;
+            }
         }
 
         // float _time = 0;
@@ -25,7 +35,7 @@ public class WaterScript : MonoBehaviour
         // }
     }
 
-    // void HealPlayer(Collision c){
+    // void HealPlayer(Collider c){
     //     c.gameObject.GetComponent<CactusGuy>().HealShield(5);
     // }
 }
