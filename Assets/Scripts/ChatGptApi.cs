cb83974996d743822a163082f0b4e52830fa46fd
DrAlip <50599661+DrAlip@users.noreply.github.com>
2023-05-27 18:23:25 -0700

    ChatGPT api added to the project

--- Assets/Scripts/ChatGptApi.cs
+++ Assets/Scripts/ChatGptApi.cs
@@ -7,17 +7,34 @@ using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 public class ChatGptApi : MonoBehaviour
 {
     private const string ApiEndpoint = "https://api.openai.com/v1/chat/completions";
-    private const string ApiKey = "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";
+    private string ApiKey;
     private const string chatModel = "gpt-3.5-turbo";
+
+    //Reading the text file containing chatgpt api key
+    public TextAsset textAsset = Resources.Load<TextAsset>("apiKey");
 
     public async Task<string> GetChatResponse(string prompt)
     {
+
         using (HttpClient client = new HttpClient())
         {
+            
+            
+            if (textAsset != null)
+            {
+                //setting the api key to the string variable
+                ApiKey = textAsset.text;
+                Debug.Log("API Key: " + ApiKey);
+            }
+            else
+            {
+                Debug.LogWarning("API Key file not found.");
+            }
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {ApiKey}");
 
             // var jsonContent = new StringContent($"{{\"model\": \"{chatModel}\", \"messages\": [{{\"role\": \"user\", \"content\": \"Make up a story about aligators\"}}]}}");
